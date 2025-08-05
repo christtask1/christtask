c5183426c85ea5d1835dce15a93ae003b2abf321
christtask1 <veselindimitrove@gmail.com>
2025-08-05 23:12:34 +0100

    Fix ESLint error: escape apostrophe in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -96,7 +96,7 @@ export default function Login() {
 
           <div className="mt-6 text-center">
             <p className="text-gray-600 dark:text-gray-300">
-              Don't have an account?{' '}
+              Don&apos;t have an account?{' '}
               <Link href="/signup" className="text-blue-600 dark:text-blue-400 hover:underline font-medium">
                 Sign up
               </Link>
