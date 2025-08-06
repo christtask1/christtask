488c01b6c51b13a584fbc5701c53a8a445ce49bd
christtask1 <veselindimitrove@gmail.com>
2025-08-06 05:48:32 +0100

    Fix deployment: simplify webpack config and add .vercelignore

--- next.config.js
+++ next.config.js
@@ -5,12 +5,6 @@ const nextConfig = {
   },
   // Exclude Supabase Edge Functions from the build
   webpack: (config) => {
-    // Exclude supabase directory from webpack
-    config.module.rules.push({
-      test: /supabase\/functions/,
-      use: 'ignore-loader'
-    })
-    
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
