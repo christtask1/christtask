28713c608645ca3062d7ca724c63c953ee1006ad
christtask1 <veselindimitrove@gmail.com>
2025-08-27 09:02:00 +0100

    Add Whop embedded checkout: Script tag in layout and payment page with embedded checkout form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,11 @@ export default function RootLayout({ children }: { children: any }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+        <script
+          async
+          defer
+          src="https://js.whop.com/static/checkout/loader.js"
+        ></script>
         <style>{`
           :root {
             --bg: #040406;
